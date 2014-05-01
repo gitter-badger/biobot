2fc6550874598ff8e876a26bb0bdf61a1f93d420
Tom Hodder <tom@limepepper.co.uk>
2014-05-01 10:46:30 +0100

    initial commit of phenny modules and temp checker

---
+++ .phenny/default.py
@@ -0,0 +1,24 @@
+nick = "biobot"
+host = "chat.freenode.net"
+password = "earth5resume5damage"
+channels = ["#london-biohackers-bot-test", "#london-biohackers"]
+owner = "tolland"
+
+# people allowed to use admin.py functions
+admins = [owner]
+
+# for available modules see /usr/share/python-support/phenny/phenny/modules
+
+# exclude modules
+exclude = ["admin"]
+
+# enable specific modules, trumps exclude
+# enable = []
+
+# extra modules to load by filename
+extra = ["/home/pi/.phenny/modules"]
+
+# limit modules in specific channels
+# limit = {"#channel": ["module-a", "module-b"]}
+
+prefix = "\?"
