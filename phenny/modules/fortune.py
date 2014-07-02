146d30374d89f76bef1fe0a6d1809e6b7cce9466
Tom Hodder <tom@limepepper.co.uk>
2014-07-02 13:43:38 +0000

    removing the phenny modules

--- phenny/modules/fortune.py
+++
@@ -1,8 +0,0 @@
-import commands
-
-def fortune(phenny, input):
-   fortune = commands.getoutput('fortune')
-   for line in fortune.rsplit("\n"):
-      phenny.say(line)
-fortune.commands = ['fortune']
-
